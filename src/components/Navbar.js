05997b8d84956065268e70c5d3921dd20ba0d44d
jHaslip <Jaredahaslip@gmail.com>
2021-02-18 19:30:37 -0600

    touch-ups

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     const [click, setClick] = useState(false)
 
-    const handleClick = () => setClick(!click)
-    
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
     return (
         <>
             <nav className="navbar">
@@ -17,7 +18,26 @@ function Navbar() {
                     <i className={click ? 'fas fa fa-times' : 'fas fa-bars'} />
                   </div>
                   <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-
+                      <li className='nav-item'>
+                        <Link to='/home' className='nav-links' onClick={closeMobileMenu}>
+                           Home
+                        </Link>
+                      </li>
+                      <li className='nav-item'>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                            Services
+                        </Link>
+                      </li>
+                      <li className='nav-item'>
+                        <Link to='products' className='nav-links' onClick={closeMobileMenu}>
+                            Products
+                        </Link>
+                      </li>
+                      <li className='nav-item'>
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                      </li>
                   </ul>
               </div>
             </nav>
